5ab6590db9eb5a4bc4e5e46d0b1665cc11d81177
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-05-08 08:28:56 +0100

    Update betRoutes.js

--- src/routes/betRoutes.js
+++ src/routes/betRoutes.js
@@ -1,9 +1,21 @@
+// const express = require('express');
+// const router = express.Router();
+// const authMiddleware = require('../middleware/auth');
+// const { getBetHistory, getBetStats } = require('../controllers/betController');
+
+// router.get('/history', authMiddleware, getBetHistory);
+// router.get('/stats', authMiddleware, getBetStats);
+
+// module.exports = router;
+
+// src/routes/betRoutes.js
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/auth');
-const { getBetHistory, getBetStats } = require('../controllers/betController');
+const { getBetHistory, getBetStats, placeBet } = require('../controllers/betController');
 
 router.get('/history', authMiddleware, getBetHistory);
 router.get('/stats', authMiddleware, getBetStats);
+router.post('/', authMiddleware, placeBet);
 
 module.exports = router;
