18640f661eb13931b5aee1017e13c31205e3b146
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-05-09 15:42:07 +0100

    Update ResetToken.js

--- src/models/ResetToken.js
+++ src/models/ResetToken.js
@@ -1,5 +1,27 @@
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcryptjs'); // Changed from 'bcrypt' to 'bcryptjs'
+
+// const resetTokenSchema = new mongoose.Schema({
+//   userId: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     required: true,
+//     ref: 'User',
+//   },
+//   token: {
+//     type: String,
+//     required: true,
+//   },
+//   expires: {
+//     type: Date,
+//     required: true,
+//   },
+// });
+
+// resetTokenSchema.pre('save', async function (next) {
+//   if (this.isModified('token')) {
+//     this.token = await bcrypt.hash(this.token, 10);
+
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs'); // Changed from 'bcrypt' to 'bcryptjs'
 
 const resetTokenSchema = new mongoose.Schema({
   userId: {
@@ -17,11 +39,9 @@ const resetTokenSchema = new mongoose.Schema({
   },
 });
 
-resetTokenSchema.pre('save', async function (next) {
-  if (this.isModified('token')) {
-    this.token = await bcrypt.hash(this.token, 10);
-  }
-  next();
-});
-
 module.exports = mongoose.model('ResetToken', resetTokenSchema);
+//   }
+//   next();
+// });
+
+// module.exports = mongoose.model('ResetToken', resetTokenSchema);
