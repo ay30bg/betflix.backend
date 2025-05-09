7a05b9853a6d0d772c6ac20e2eaac8a5aaf26c4b
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-05-09 16:03:31 +0100

    Update ResetToken.js

--- src/models/ResetToken.js
+++ src/models/ResetToken.js
@@ -1,26 +1,3 @@
-// const mongoose = require('mongoose');
-// const bcrypt = require('bcryptjs'); // Changed from 'bcrypt' to 'bcryptjs'
-
-// const resetTokenSchema = new mongoose.Schema({
-//   userId: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     required: true,
-//     ref: 'User',
-//   },
-//   token: {
-//     type: String,
-//     required: true,
-//   },
-//   expires: {
-//     type: Date,
-//     required: true,
-//   },
-// });
-
-// resetTokenSchema.pre('save', async function (next) {
-//   if (this.isModified('token')) {
-//     this.token = await bcrypt.hash(this.token, 10);
-
 const mongoose = require('mongoose');
 
 const resetTokenSchema = new mongoose.Schema({
@@ -40,8 +17,3 @@ const resetTokenSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('ResetToken', resetTokenSchema);
-//   }
-//   next();
-// });
-
-// module.exports = mongoose.model('ResetToken', resetTokenSchema);
