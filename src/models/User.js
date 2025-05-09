4036cffcd9d495c22b391a7816bf92d6be9276ef
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-05-09 10:54:08 +0100

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,30 +1,3 @@
-// const mongoose = require('mongoose');
-// const bcrypt = require('bcryptjs'); // Changed from 'bcrypt' to 'bcryptjs'
-
-// const userSchema = new mongoose.Schema({
-//   username: { type: String, required: true, unique: true, trim: true },
-//   email: { type: String, required: true, unique: true, trim: true, lowercase: true },
-//   password: { type: String, required: true },
-//   balance: { type: Number, default: 0.0 },
-//   createdAt: { type: Date, default: Date.now },
-//   updatedAt: { type: Date, default: Date.now },
-// }, { timestamps: true });
-
-// // Hash password before saving
-// userSchema.pre('save', async function (next) {
-//   if (this.isModified('password')) {
-//     this.password = await bcrypt.hash(this.password, 10);
-//   }
-//   next();
-// });
-
-// // Method to compare passwords
-// userSchema.methods.comparePassword = async function (candidatePassword) {
-//   return await bcrypt.compare(candidatePassword, this.password);
-// };
-
-// module.exports = mongoose.model('User', userSchema);
-
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
