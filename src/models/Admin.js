d6d7a4b703235c9110e991a2af5eeb7e71239e94
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-05-10 14:22:44 +0100

    Update Admin.js

--- src/models/Admin.js
+++ src/models/Admin.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const adminSchema = new mongoose.Schema({
   email: {
@@ -19,5 +20,19 @@ const adminSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
+
+// Hash password before saving
+adminSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
+});
+
+// Method to compare passwords
+adminSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model('Admin', adminSchema);
