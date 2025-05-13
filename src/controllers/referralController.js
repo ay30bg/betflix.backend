3c399e67583ae3db072ee10ed65f0366e7b06ac1
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-05-13 21:50:42 +0100

    Update referralController.js

--- src/controllers/referralController.js
+++ src/controllers/referralController.js
@@ -1,28 +1,3 @@
-// const Referral = require('../models/Referral');
-// const { v4: uuidv4 } = require('uuid');
-
-// const getReferralLink = async (req, res) => {
-//   try {
-//     let referral = await Referral.findOne({ referrerId: req.user.id });
-
-//     if (!referral) {
-//       referral = await Referral.create({
-//         referrerId: req.user.id,
-//         code: uuidv4(),
-//       });
-//     }
-
-//     const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
-//     const referralLink = `${baseUrl}/sign-up?ref=${referral.code}`;
-
-//     res.json({ referralLink });
-//   } catch (err) {
-//     res.status(500).json({ error: 'Failed to generate referral link' });
-//   }
-// };
-
-// module.exports = { getReferralLink };
-
 const Referral = require('../models/Referral');
 
 // Function to generate an 8-digit numeric code
