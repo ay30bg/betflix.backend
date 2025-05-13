8b922266ce289c8114b5d30432cf87051c93a831
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-05-13 21:49:20 +0100

    Update Referral.js

--- src/models/Referral.js
+++ src/models/Referral.js
@@ -1,33 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const referralSchema = new mongoose.Schema({
-//   referrerId: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'User',
-//     required: true,
-//     index: true,
-//   },
-//   referredUsers: [{
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'User',
-//   }],
-//   code: {
-//     type: String,
-//     required: true,
-//     unique: true,
-//   },
-//   createdAt: {
-//     type: Date,
-//     default: Date.now,
-//   },
-//   expiresAt: {
-//     type: Date,
-//     default: () => new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30-day expiration
-//   },
-// });
-
-// module.exports = mongoose.model('Referral', referralSchema);
-
 const mongoose = require('mongoose');
 
 const referralSchema = new mongoose.Schema({
