34c5e1ca89a74ce986f86ce841dae9ee8de9eb0f
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-05-15 16:44:03 +0100

    Update adminRoutes.js

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
+const { getRoundResult } = require('../controllers/betController');
 const adminAuthMiddleware = require('../middleware/adminAuth');
 
 // Public routes
@@ -12,6 +13,7 @@ router.post('/reset-password/:token', adminController.resetPassword);
 // Protected routes (require admin authentication)
 router.get('/dashboard', adminAuthMiddleware, adminController.getDashboard);
 router.get('/users', adminAuthMiddleware, adminController.getAllUsers);
+router.get('/rounds/result/:period', authenticateAdminToken, getRoundResult);
 router.put('/users/:userId', adminAuthMiddleware, adminController.editUser);
 router.put('/users/:userId/ban', adminAuthMiddleware, adminController.toggleBanUser);
 router.delete('/users/:userId', adminAuthMiddleware, adminController.deleteUser);
