d48f85402593029607b2c8345aab25b32ed9fa43
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-05-17 07:52:43 +0100

    Update adminRoutes.js

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,25 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const adminController = require('../controllers/adminController');
-// // const { getRoundResult } = require('../controllers/betController');
-// const adminAuthMiddleware = require('../middleware/adminAuth');
-
-// // Public routes
-// router.post('/signup', adminController.signupAdmin);
-// router.post('/login', adminController.loginAdmin);
-// router.post('/forgot-password', adminController.forgotPassword);
-// router.post('/reset-password/:token', adminController.resetPassword);
-
-// // Protected routes (require admin authentication)
-// router.get('/dashboard', adminAuthMiddleware, adminController.getDashboard);
-// router.get('/users', adminAuthMiddleware, adminController.getAllUsers);
-// // router.get('/rounds/result/:period', authenticateAdminToken, getRoundResult);
-// router.put('/users/:userId', adminAuthMiddleware, adminController.editUser);
-// router.put('/users/:userId/ban', adminAuthMiddleware, adminController.toggleBanUser);
-// router.delete('/users/:userId', adminAuthMiddleware, adminController.deleteUser);
-
-// module.exports = router;
-
 const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
@@ -36,9 +14,9 @@ router.post('/reset-password/:token', adminController.resetPassword);
 router.get('/dashboard', adminAuthMiddleware, adminController.getDashboard);
 router.get('/users', adminAuthMiddleware, adminController.getAllUsers);
 // router.get('/rounds/result/:period', authenticateAdminToken, getRoundResult);
-router.post('/rounds', adminAuthMiddleware, adminController.createRound); // New route for createRound
 router.put('/users/:userId', adminAuthMiddleware, adminController.editUser);
 router.put('/users/:userId/ban', adminAuthMiddleware, adminController.toggleBanUser);
 router.delete('/users/:userId', adminAuthMiddleware, adminController.deleteUser);
 
 module.exports = router;
+
