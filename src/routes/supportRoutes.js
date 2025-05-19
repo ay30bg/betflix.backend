adcf59a47368168a85866e890c00f7f424c5d0fd
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-05-19 16:00:23 +0100

    Update supportRoutes.js

--- src/routes/supportRoutes.js
+++ src/routes/supportRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const { getMessages, createMessage, addReply } = require('../controllers/supportController');
-const authMiddleware = require('../middleware/auth');
+const authMiddleware = require('../middleware/authMiddleware');
 const adminAuthMiddleware = require('../middleware/adminAuth');
 
 // Get messages
