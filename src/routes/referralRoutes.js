a777e2ad16783f2e00e9b48e770462b4179f517b
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-05-20 08:58:00 +0100

    Update referralRoutes.js

--- src/routes/referralRoutes.js
+++ src/routes/referralRoutes.js
@@ -1,8 +1,20 @@
+// const express = require('express');
+// const router = express.Router();
+// const authMiddleware = require('../middleware/auth');
+// const { getReferralLink } = require('../controllers/referralController');
+
+// router.get('/link', authMiddleware, getReferralLink);
+
+// module.exports = router;
+
+// routes/referral.js
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/auth');
-const { getReferralLink } = require('../controllers/referralController');
+const { getReferralLink, getReferralStats, withdrawReferralBonus } = require('../controllers/referralController');
 
 router.get('/link', authMiddleware, getReferralLink);
+router.get('/stats', authMiddleware, getReferralStats);
+router.post('/withdraw-bonus', authMiddleware, withdrawReferralBonus);
 
 module.exports = router;
