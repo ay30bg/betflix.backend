a21c31d37923af4479f5040d99d0b550078156fd
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-05-22 19:42:52 +0100

    Update nowpayments.js

--- src/config/nowpayments.js
+++ src/config/nowpayments.js
@@ -4,5 +4,6 @@ module.exports = {
   apiKey: process.env.NOWPAYMENTS_API_KEY,
   baseUrl: 'https://api.nowpayments.io/v1',
   webhookSecret: process.env.NOWPAYMENTS_WEBHOOK_SECRET,
-  email: process.env.NOWPAYMENTS_EMAIL // Add this
+  email: process.env.NOWPAYMENTS_EMAIL ,
+  password: process.env.NOWPAYMENTS_PASSWORD
 };
